0291968bd814b6908f31f4504198341a12b4b761
Luckyburgs <kyogabirwelucky8@gmail.com>
2024-10-10 12:48:42 +0300

    Create Lucky .java

---
+++ Lucky .java
@@ -0,0 +1,13 @@
+// import java.util.Scanner;
+
+// prompt stays on the same line with the printed text after the program executes successfully.
+public class code001{
+        public static void main(String[] args){
+                // Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Hello, ");
+                System.out.print("World !");
+
+                // scanner.close();
+        }
+}
